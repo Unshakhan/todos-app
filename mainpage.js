349bb384a134ce67bd784e33076cb1c13ab49a8b
Unshakhan <unshak08@gmail.com>
2026-02-12 13:36:27 +0500

    update

--- mainpage.js
+++ mainpage.js
@@ -22,6 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
             : '0%';
 
         progressNum.textContent = `${completedTasks} / ${totalTasks}`;
+
+        if( totalTasks > 0 && completedTasks === totalTasks){
+            launchConfetti()
+        }
     }
 
     const addTask = async (text, completed = false, id = null) => {
@@ -175,3 +179,44 @@ logoutBtn.addEventListener("click", async () => {
 
 })
 
+const launchConfetti = () =>{
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
